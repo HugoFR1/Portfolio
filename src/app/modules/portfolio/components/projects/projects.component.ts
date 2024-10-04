f28c3b83c93cb240a8b5efa0645e17fdc8c63b2c
DESKTOP-E79UVIF\Vanessa <huguinhofraga@gmail.com>
2024-10-04 15:59:25 -0300

    Criação interface e assets

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Iprojects } from '../../interface/IProjects.interface';
 
 @Component({
   selector: 'app-projects',
@@ -8,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
-
+  public arrayProjects = signal<Iprojects[]>([
+    {
+      src: 'assets/img/projects/vfull.png',
+      alt: 'Projeto indisponível',
+      title: 'vida fullstack',
+      width: '100px',
+      height: '51px',
+      description: 
+      '<p>no description<p/>',
+      links: [
+        {
+          name: 'conheça o blog(não tem blog)',
+          href: 'https://github.com/HugoFR1'
+        },
+      ],
+    },
+  ]);
 }
