983a5da5a8e714d708aeeea5ca55a784acad2d30
DESKTOP-E79UVIF\Vanessa <huguinhofraga@gmail.com>
2024-10-18 14:58:14 -0300

    Terminei

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -22,15 +22,15 @@ export class ProjectsComponent {
     {
       src: 'assets/img/projects/vfull.png',
       alt: 'Projeto Vida FullStack',
-      title: 'Vida FullStack',
+      title: 'Projetos disponíveis',
       with: '100px',
       height: '51px',
       description:
-        '<p>Explore o fascinante mundo do desenvolvimento web no meu blog dedicado exclusivamente ao ecossistema Angular, Front-end, JavaScript, Node.js e TypeScript.</p> <p>Desvende os segredos por trás da construção de interfaces modernas, aprimore suas habilidades de programação front-end quanto back-end e mergulhe em conceitos avançados de JavaScript e TypeScript.</p>',
+        '<p>No momento não tenho projetos publicos para compartilhar</p>',
       links: [
         {
-          name: 'Conheça o Blog',
-          href: 'https://vidafullstack.com.br',
+          name: 'Veja meu GitHub',
+          href: 'https://github.com/HugoFR1',
         },
       ],
     },
